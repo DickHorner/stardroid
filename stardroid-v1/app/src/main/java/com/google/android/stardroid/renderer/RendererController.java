97274eb2c547d58a5d1182e81ee6e163cfe5730e
J Luetkens <megahoschi@googlemail.com>
2026-07-28 20:27:44 +0200

    Use shared executor for PushNav target delivery

--- stardroid-v1/app/src/main/java/com/google/android/stardroid/renderer/RendererController.java
+++ stardroid-v1/app/src/main/java/com/google/android/stardroid/renderer/RendererController.java
@@ -14,10 +14,22 @@
 
 package com.google.android.stardroid.renderer;
 
+import android.content.SharedPreferences;
 import android.opengl.GLSurfaceView;
+
+import com.google.android.stardroid.ApplicationConstants;
+import com.google.android.stardroid.math.RaDec;
+import com.google.android.stardroid.math.Vector3;
+import com.google.android.stardroid.pushnav.PushNavTargetSender;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.ScheduledExecutorService;
+
+import dagger.hilt.EntryPoint;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.EntryPointAccessors;
+import dagger.hilt.components.SingletonComponent;
 
 /**
  * Allows the rest of the program to communicate with the SkyRenderer by queueing
@@ -25,6 +37,13 @@ import java.util.Queue;
  * @author James Powell
  */
 public class RendererController extends RendererControllerBase {
+  @EntryPoint
+  @InstallIn(SingletonComponent.class)
+  interface PushNavEntryPoint {
+    SharedPreferences sharedPreferences();
+    ScheduledExecutorService backgroundExecutor();
+  }
+
   /**
    * Used for grouping renderer calls into atomic units.
    */
@@ -65,6 +84,8 @@ public class RendererController extends RendererControllerBase {
   }
 
   private final EventQueuer mQueuer;
+  private final SharedPreferences sharedPreferences;
+  private final ScheduledExecutorService backgroundExecutor;
 
   @Override
   protected EventQueuer getQueuer() {
@@ -74,6 +95,20 @@ public class RendererController extends RendererControllerBase {
   public RendererController(SkyRenderer renderer, final GLSurfaceView view) {
     super(renderer);
     mQueuer = view::queueEvent;
+    PushNavEntryPoint entryPoint = EntryPointAccessors.fromApplication(
+        view.getContext().getApplicationContext(), PushNavEntryPoint.class);
+    sharedPreferences = entryPoint.sharedPreferences();
+    backgroundExecutor = entryPoint.backgroundExecutor();
+  }
+
+  @Override
+  public void queueEnableSearchOverlay(final Vector3 target, final String targetName) {
+    RaDec targetRaDec = RaDec.fromGeocentricCoords(target);
+    String serverUrl = sharedPreferences.getString(
+        ApplicationConstants.PUSHNAV_SERVER_URL_PREF_KEY, "");
+    PushNavTargetSender.sendAsync(
+        backgroundExecutor, serverUrl, targetRaDec.getRa(), targetRaDec.getDec());
+    super.queueEnableSearchOverlay(target, targetName);
   }
 
   @Override
